9b1f37624c2052863bab17ffb7f9a2d694dd00f4
Admin <Admin@ADMIN->
2021-11-02 17:09:31 +0300

    практика

--- Notes/Notes/AssemblyInfo.cs
+++ Notes/Notes/AssemblyInfo.cs
@@ -1,3 +1,30 @@
-using Xamarin.Forms.Xaml;
+using System;
+using System.IO;
+using Xamarin.Forms;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Notes
+{
+    public partial class App : Application
+    {
+        public static string FolderPath { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
